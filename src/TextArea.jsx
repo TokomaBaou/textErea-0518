87bf04e4529ffc5205a31c4fb505e8d4b47495b3
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-18 23:53:13 +0900

    stateの設定

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+
 export const TextArea = () => {
-  return <>text areadです</>;
+  const [text, setText] = useState("");
+  return (
+    <>
+      text areadです
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </>
+  );
 };
